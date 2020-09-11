5d173054d585ddaa1da40805265b7975a96a92e7
jonipitkaranta <69782878+jonipitkaranta@users.noreply.github.com>
2020-09-11 11:35:05 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,28 @@
+import random
+
+
+def lotto():
+    """Yksinkertainen lottopeli
+    
+       Numerot arvotaan tyhjään listaan
+
+       Arvotaan satunnaiset numerot ja tarkistetaan ovatko ne jo arvottu
+
+       Satunnaiset numerot appendataan tyhjään listaan ja sortataan pienimmästä suurimpaan
+    """
+    lottonumerot = []
+    for i in range(0, 6):
+        number = random.randint(1, 40)
+        while number in lottonumerot:
+            number = random.randint(1, 40)
+
+        lottonumerot.append(number)
+
+    lottonumerot.sort()
+
+    print('>>> Lottonumerot ovat <<<')
+    print(lottonumerot)
+
+
+lotto()
+print(lotto().__doc__)
